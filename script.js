a4553bbc22464fdba5df744ab286383bed8b2841
tallys augusto <tallysmugetsu@gmail.com>
2019-09-06 21:46:10 -0300

    testando js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+$(document).ready(function(){
+   
+    $('.red').click(function(){
+        $(this).fadeOut();
+    
+});
 
+    $('.green').click(function(){
+        $('.blue').hide();
+        
+    });
+
+    $('.yellow').click(function(){
+        $('.green').hide();
+        $('.blue').fadeIn();
+        
+    });
+
+    $('.blue').click(function(){
+        $('.blue, .red, .yellow, .green').fadeToggle();
+    });
+    
+    $('.black').click(function(){
+        $('.blue, .red, .yellow, .green').toggle("slow");
+    });
+});
